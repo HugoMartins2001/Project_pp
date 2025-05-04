0b5c5cc3f1b70fd440f9b358387ed71ce11a025a
Hugo Martins <hugolm280@hotmail.com>
2025-05-04 17:13:58 +0100

    adicionar o team no GoalEvent pois nao tinha

--- src/event/GoalEvent.java
+++ src/event/GoalEvent.java
@@ -2,6 +2,8 @@ package event;
 
 import com.ppstudios.footballmanager.api.contracts.event.IGoalEvent;
 import com.ppstudios.footballmanager.api.contracts.player.IPlayer;
+import com.ppstudios.footballmanager.api.contracts.team.ITeam;
+
 import java.io.IOException;
 
 /**
@@ -12,8 +14,8 @@ public class GoalEvent extends Event implements IGoalEvent{
 
     private IPlayer player;
 
-    public GoalEvent(IPlayer player, String description, int minute) {
-        super(description,minute);
+    public GoalEvent(IPlayer player, String description, int minute, ITeam team) {
+        super(description,minute,team);
         this.player = player;
     }
 
